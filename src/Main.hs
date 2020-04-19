f50c4e8f0eb1414f54ded29609a37e94eceddef2
Vicfred <vicfred@vicfred.dev>
2020-04-18 19:18:03 -0500

    send msg, process input, set info

--- src/Main.hs
+++ src/Main.hs
@@ -6,14 +6,17 @@ import qualified Network.Socket as N  -- network
 -- Configuration options
 myServer = "irc.freenode.org" :: String
 myPort   = 6667 :: N.PortNumber
+myChan   = "#meowbot-testing" :: String
+myNick   = "meowbot_haskell" :: String
 
 -- Toplevel program
 main :: IO ()
 main = do
     h <- connectTo myServer myPort
-    t <- hGetContents h
-    hSetBuffering stdout NoBuffering
-    print t
+    write h "NICK" myNick
+    write h "USER" (myNick ++ " 0 * :tutorial bot")
+    write h "JOIN" myChan
+    listen h
 
 -- Connect to a server given its name and port number
 connectTo :: N.HostName -> N.PortNumber -> IO Handle
@@ -22,4 +25,20 @@ connectTo host port = do
     sock <- N.socket (N.addrFamily addr) (N.addrSocketType addr) (N.addrProtocol addr)
     N.connect sock (N.addrAddress addr)
     N.socketToHandle sock ReadWriteMode
+
+-- Send a message to a handle
+write :: Handle -> String -> String -> IO ()
+write h cmd args = do
+    let msg = cmd ++ " " ++ args ++ "\r\n"
+    hPutStr h msg          -- Send message on the wire
+    putStr ("> " ++ msg)   -- Show sent message on the command line
+
+-- Process each line from the server
+listen :: Handle -> IO ()
+listen h = forever $ do
+    line <- hGetLine h
+    putStrLn line
+  where
+    forever :: IO () -> IO ()
+    forever a = do a; forever a
 
