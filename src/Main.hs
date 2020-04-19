96b253c1929518a0dde7b813bdc8df9fa2243ef2
Vicfred <vicfred@vicfred.dev>
2020-04-18 19:13:59 -0500

    connect to irc server

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,25 @@
 module Main where
 
+import System.IO                      -- base
+import qualified Network.Socket as N  -- network
+
+-- Configuration options
+myServer = "irc.freenode.org" :: String
+myPort   = 6667 :: N.PortNumber
+
+-- Toplevel program
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    h <- connectTo myServer myPort
+    t <- hGetContents h
+    hSetBuffering stdout NoBuffering
+    print t
+
+-- Connect to a server given its name and port number
+connectTo :: N.HostName -> N.PortNumber -> IO Handle
+connectTo host port = do
+    addr : _ <- N.getAddrInfo Nothing (Just host) (Just (show port))
+    sock <- N.socket (N.addrFamily addr) (N.addrSocketType addr) (N.addrProtocol addr)
+    N.connect sock (N.addrAddress addr)
+    N.socketToHandle sock ReadWriteMode
+
